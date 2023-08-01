c05a8c5e0d5ac919e2566b9e7eb735031d9e7d91
Pola Mounir Samir <polamounir103@gmail.com>
2023-08-01 13:14:58 +0300

    1st commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+    // var copyBtn = document.querySelectorAll("#copy-btn")
+    // // var copyPopup =document.querySelectorAll("#copy-popup")
+    // // function hover(){
+    // //     copyPopup.innerHTML="Press To Copy"
+    // //     copyPopup.style.display= "block"
+
+    // // }
+    // function press(){
+    //     // copyPopup.innerHTML="Copied"
+    //     alert("text copied")
+
+    // }
+    // copyBtn.addEventListener(onclick , press)
+    // // function hide(){
+    // //     copyPopup.innerHTML=""
+    // //     copyPopup.style.display= "none"
+
+
+    // // }
+
+    // // copyBtn.onmouseenter= hover
+    // // copyBtn.onclick = press
+    // // copyBtn.onmouseleave = hide
+
+
+
+
+    function coping(){
+        alert("Text Copied")
+    }
+
+
